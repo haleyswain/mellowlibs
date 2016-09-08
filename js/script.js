9ecfd50e85e44d9f7e6856b386fe1020196cf084
haley swain and omid ashtari <haley@>
2016-09-07 20:33:41 -0700

    add a letter, that is poorly written

--- js/script.js
+++ js/script.js
@@ -5,14 +5,23 @@ $(document).ready(function(){
     var verbInput = $("input#verb").val();
     var adjectiveInput = $("input#adjective").val();
     var nounInput = $("input#noun").val();
+    var animal2Input = $("input#animal2").val();
+    var adjective2Input = $("input#adjective2").val();
+    var noun2Input = $("input#noun2").val();
 
     $(".human1").text(human1Input);
     $(".animal1").text(animal1Input);
     $(".verb").text(verbInput);
     $(".adjective").text(adjectiveInput);
     $(".noun").text(nounInput);
+    $(".animal2").text(animal2Input);
+    $(".adjective2").text(adjective2Input);
+    $(".noun2").text(noun2Input);
+
 
     $("#story").show();
+    $("#letter").show();
+
 
     event.preventDefault();
   });
